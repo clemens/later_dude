05d1179805a35ae76c666a2d62e85a7e5a3c3639
Clemens Kofler <clemens@railway.at>
2011-10-04 17:01:11 -0500

    put calendar helper methods in a helper object for cleaner testing

--- test/calendar_helper_test.rb
+++ test/calendar_helper_test.rb
@@ -1,24 +1,29 @@
 require 'test_helper'
 
 class CalendarHelperTest < ActiveSupport::TestCase
-  include LaterDude::CalendarHelper
+  setup do
+    @helper = Object.new
+    class << @helper
+      include LaterDude::CalendarHelper
+    end
+  end
 
   test "requires year and month" do
-    assert_raises(ArgumentError) { calendar_for }
-    assert_raises(ArgumentError) { calendar_for(2009) }
-    assert_nothing_raised { calendar_for(2009, 1) }
+    assert_raises(ArgumentError) { @helper.calendar_for }
+    assert_raises(ArgumentError) { @helper.calendar_for(2009) }
+    assert_nothing_raised { @helper.calendar_for(2009, 1) }
   end
 
   test "accepts optional options hash" do
     options = { :calendar_class => "my_calendar", :first_day_of_week => 1 }
-    assert_nothing_raised { calendar_for(2009, 1, options) }
+    assert_nothing_raised { @helper.calendar_for(2009, 1, options) }
   end
 
   test "accepts optional block" do
     options = { :calendar_class => "my_calendar", :first_day_of_week => 1 }
-    some_block = lambda { nil }
+    some_block = lambda { |day| nil }
 
-    assert_nothing_raised { calendar_for(2009, 1, &some_block) }
-    assert_nothing_raised { calendar_for(2009, 1, options, &some_block) }
+    assert_nothing_raised { @helper.calendar_for(2009, 1, &some_block) }
+    assert_nothing_raised { @helper.calendar_for(2009, 1, options, &some_block) }
   end
 end
